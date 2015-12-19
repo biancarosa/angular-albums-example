786ea169fb9ea2ca990122b35c42d6eaccc1ddb6
Bianca Rosa <me@biancarosa.com.br>
2015-12-18 23:47:50 -0200

    minimum app working

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 This is an example of an angular app for learning (& showcasing) purposes.
 
+#Running the project
+1. npm install
+2. npm start
+
+#How it works
+Given a word, searchs for albums in spotify with that word
+
 #Decisions
 - CDN: I am using Angular from Google's CDN.
 - One file for each kind of dependency(one controller, another for filters, and so it goes...): This is a sample project. Depending on the size of your project, you might wanna go for separate files for each controller, service or whatsoever.
--- js/controllers.js
+++ js/controllers.js
@@ -3,9 +3,18 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('ListAlbumsController', function ($scope) {
-    // write Ctrl here
-    console.log('hi');
-
+  controller('ListAlbumsController', function ($scope, $http) {
+    $scope.doSearch = function() {
+      if ($scope.search.length > 1) {
+        $http({
+          method: 'GET',
+          url: 'https://api.spotify.com/v1/search?q='+$scope.search+'&type=album&limit=21'
+        }).then(function successCallback(response) {
+          $scope.albums = response.data.albums.items;
+          console.log($scope.albums);
+        }, function errorCallback(response) {
+          console.log(response);
+        });
+      }
+    }
   });
-
